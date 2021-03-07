174298a9c1448076395b54e8f3438650d73e7f14
edutalkapp <binh@edutalkapp.com>
2021-03-07 21:32:02 +0700

    migrations

--- src/PMVOnline.EntityFrameworkCore.DbMigrations/Migrations/20210307143141_AddTaskNotification.Designer.cs
+++ src/PMVOnline.EntityFrameworkCore.DbMigrations/Migrations/20210307143141_AddTaskNotification.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PMVOnline.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -9,9 +10,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace PMVOnline.Migrations
 {
     [DbContext(typeof(PMVOnlineMigrationsDbContext))]
-    partial class PMVOnlineMigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210307143141_AddTaskNotification")]
+    partial class AddTaskNotification
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
