e75b316013377c2652435e4d4d44b55ad5d50f76
edutalkapp <binh@edutalkapp.com>
2021-07-04 15:18:48 +0700

    add mirate

---
+++ src/PMVOnline.EntityFrameworkCore.DbMigrations/Migrations/20210626074728_NotNullForLeader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PMVOnline.Migrations
+{
+    public partial class NotNullForLeader : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppTasks_AppAppUsers_LeaderId",
+                table: "AppTasks");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LeaderId",
+                table: "AppTasks",
+                type: "char(36)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "char(36)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppTasks_AppAppUsers_LeaderId",
+                table: "AppTasks",
+                column: "LeaderId",
+                principalTable: "AppAppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppTasks_AppAppUsers_LeaderId",
+                table: "AppTasks");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LeaderId",
+                table: "AppTasks",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppTasks_AppAppUsers_LeaderId",
+                table: "AppTasks",
+                column: "LeaderId",
+                principalTable: "AppAppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
